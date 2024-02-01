c524629de71844eb580db83fe2b9430b9051806b
Olena Dmytriieva <dmitriyevalena5@gmail.com>
2024-02-01 14:47:43 +0000

    Added Authentication and exclusions

--- src/main/java/com/commerce/store/ecommerce/backend/api/security/WebSecurityConfig.java
+++ src/main/java/com/commerce/store/ecommerce/backend/api/security/WebSecurityConfig.java
@@ -5,16 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
 
-//    When someone send requst to  access to rest api when we process it, if we have Security filterChain it goes through
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    //    When someone send requst to  access to rest api when we process it, if we have Security filterChain it goes through
 //     to verify if request is valid before putting it through the controller
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf().disable().cors().disable();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+//        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.authorizeHttpRequests()
+        .requestMatchers("/product","/auth/register","auth/login").permitAll()
+//                .requestMatchers("/auth/register").permitAll()
+//                .requestMatchers("auth/login").permitAll()
+        .anyRequest().authenticated();
         return http.build();
     }
 }
